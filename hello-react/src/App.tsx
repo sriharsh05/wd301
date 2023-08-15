30967012e0f7ff42ceae74cff49e6171ce9053e0
sriharsh05 <sriharsh.harsha5@gmail.com>
2023-08-15 19:29:33 +0530

    changes done based on rejection

--- hello-react/src/App.tsx
+++ hello-react/src/App.tsx
@@ -11,13 +11,13 @@ function App() {
       <div className="grid grid-cols-2 gap-8 p-8">
       <div className="border rounded p-4 border-lime-700 ">
         <h1>Pending</h1>
-        <TaskCard title = "Build the website with static content" duedate="10th Aug" assignee="Sriharsh" flag="pending"/>
-        <TaskCard title = "Prepare for inteview" duedate="11th Aug" assignee="Sriharsh" flag="pending"/>
+        <TaskCard title = "Build the website with static content" dueDate="10th Aug" assigneeName="Sriharsh" />
+        <TaskCard title = "Prepare for inteview" dueDate="11th Aug" assigneeName="Sriharsh" />
       </div>
       <div className="border rounded p-4 border-lime-700">  
         <h1>Done</h1>
-        <TaskCard title = "Submit The project" completedate="7th Aug" assignee="Sriharsh" flag="done"/>
-        <TaskCard title = "Driving classes application" completedate="1st Aug" assignee="Sriharsh" flag="done"/>
+        <TaskCard title = "Submit The project" completedAtDate="7th Aug" assigneeName="Sriharsh" />
+        <TaskCard title = "Driving classes application" completedAtDate="1st Aug" assigneeName="Sriharsh" />
       </div>
       </div>
     </>
--- hello-react/src/TaskCard.tsx
+++ hello-react/src/TaskCard.tsx
@@ -2,32 +2,28 @@ import React from 'react';
 import './TaskCard.css'
 
 interface TaskCardProps {
-  flag: 'pending' | 'done';
   title: string;
-  duedate?: string;
-  completedate?: string;
-  assignee: string;
+  dueDate?: string;
+  completedAtDate?: string;
+  assigneeName: string;
 }
 
 const TaskCard = (props: TaskCardProps) => {
-  if(props.flag === "pending"){
+  let displayDate = ""
+  if(props.dueDate){
+     displayDate ="Due on: " + props.dueDate;
+  }
+  else {
+    displayDate = "Completed on: "+ props.completedAtDate;
+  }
+  
   return (
     <div className="TaskItem">
       <h2 className="text-xl font-bold">{props.title}</h2>
-      <p>Due on: {props.duedate}</p>
-      <p>Assignee: {props.assignee}</p>
+      <p> {displayDate}</p>
+      <p>Assignee: {props.assigneeName}</p>
     </div>  
   )
-  }
-  else if(props.flag === "done"){
-    return (
-        <div className="TaskItem">
-          <h2 className="text-xl font-bold">{props.title}</h2>
-          <p>Completed on: {props.completedate}</p>
-          <p>Assignee: {props.assignee}</p>
-        </div>
-    )
-  }
 }
 
 export default TaskCard;
