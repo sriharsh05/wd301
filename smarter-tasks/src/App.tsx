7518a596869dc838a2baf093d14649d135a485ba
sriharsh05 <sriharsh.harsha5@gmail.com>
2023-08-31 15:32:29 +0530

    handle bugs

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,12 +1,10 @@
 import "./App.css";
-import TaskList from "./TaskList";
 import TaskApp from "./TaskApp";
 
 function App() {
   return (
     <div className="App">
       <TaskApp />
-      <TaskList tasks={[]} />
     </div>
   );
 }
