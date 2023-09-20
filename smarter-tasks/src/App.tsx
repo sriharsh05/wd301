205707c6fb1522d20203dc2ca77e32be7302c7bf
sriharsh05 <sriharsh.harsha5@gmail.com>
2023-09-20 16:48:25 +0530

    add signup and signup page for PMS and delete unnecessary task files.

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,54 +1,39 @@
-import {
-  createBrowserRouter,
-  Navigate,
-  RouterProvider,
-} from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import TaskListPage from "./pages/TaskListPage";
-import TaskDetailsPage from "./pages/TaskDetailsPage";
-import Signin from "./pages/Signin";
-import ProtectedRoute from "./ProtectedRoute";
-import Layout from "./Layout";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Notfound from "./pages/Notfound";
+import Signup from "./pages/signup";
+import Signin from "./pages/signin";
+import Dashboard from "./pages/dashboard";
+import ProtectedRoute from "./ProtectedRoute";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Navigate to="/signin" replace />,
+    element: <Signup />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
   },
   {
     path: "/signin",
     element: <Signin />,
   },
   {
-		path: "/notfound",
-		element: <Notfound />,
-	},
+    path: "/notfound",
+    element: <Notfound />,
+  },
   {
+    path: "/dashboard",
     element: (
       <ProtectedRoute>
-        <Layout />
+        <Dashboard />
       </ProtectedRoute>
     ),
-    children: [
-      {
-        path: "home",
-        element: <HomePage />,
-      },
-      {
-        path: "tasks",
-        element: <TaskListPage />,
-      },
-      {
-        path: "tasks/:id",
-        element: <TaskDetailsPage />,
-      },
-      {
-        path: "*",
-        element: <Navigate to="/notfound" replace />
-      }
-    ],
-  }
+  },
+  {
+    path: "*",
+    element: <Notfound />,
+  },
 ]);
 
 const App = () => {
