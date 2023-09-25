c2eaf7978b6edb7d5c75d9c01f2d1c937545ddd6
sriharsh05 <sriharsh.harsha5@gmail.com>
2023-09-25 15:20:23 +0530

    add modal to create form

--- smarter-tasks/src/pages/projects/index.tsx
+++ smarter-tasks/src/pages/projects/index.tsx
@@ -1,8 +1,13 @@
 import ProjectList from "./ProjectList";
+import NewProject from "./NewProject";
+
 const Projects = () => {
   return (
     <>
-      <h2>Projects</h2>
+      <div className="flex justify-between">
+        <h2 className="text-2xl font-medium tracking-tight">Projects</h2>
+        <NewProject />
+      </div>
       <ProjectList />
     </>
   )
