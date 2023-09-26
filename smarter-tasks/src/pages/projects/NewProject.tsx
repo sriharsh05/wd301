364072b68a3caa23056aef0fe39659e0f928e58b
sriharsh05 <sriharsh.harsha5@gmail.com>
2023-09-26 14:55:49 +0530

    add task feature implemented

--- smarter-tasks/src/pages/projects/NewProject.tsx
+++ smarter-tasks/src/pages/projects/NewProject.tsx
@@ -1,8 +1,10 @@
 import { Dialog, Transition } from "@headlessui/react";
 import { Fragment, useState } from "react";
+import { API_ENDPOINT } from "../../config/constants";
 
 const NewProject = () => {
   let [isOpen, setIsOpen] = useState(false);
+  const [name, setName] = useState("");
 
   const openModal = () => {
     setIsOpen(true);
@@ -10,6 +12,38 @@ const NewProject = () => {
 
   const closeModal = () => {
     setIsOpen(false);
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const token = localStorage.getItem("authToken") ?? "";
+    try {
+      const response = await fetch(`${API_ENDPOINT}/projects`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({ name }),
+      });
+      // If response is not OK, in that case I'll throw an error.
+
+      if (!response.ok) {
+        throw new Error("Failed to create project");
+      }
+      // Next, I'll extract the response body as JSON data
+
+      const data = await response.json();
+
+      // Let's print the data in console
+      setIsOpen(false);
+
+      console.log(data);
+    } catch (error) {
+      // And in catch block, I'll print the error in console.
+
+      console.error("Operation failed:", error);
+    }
   };
 
   return (
@@ -52,6 +86,34 @@ const NewProject = () => {
                   >
                     Create new project
                   </Dialog.Title>
+                  <div className="mt-2">
+                    <form onSubmit={handleSubmit}>
+                      <input
+                        type="text"
+                        required
+                        placeholder="Enter project name..."
+                        autoFocus
+                        name="name"
+                        id="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        className="w-full border rounded-md py-2 px-3 my-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue"
+                      />
+                      <button
+                        type="submit"
+                        className="inline-flex justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 mr-2 text-sm font-medium text-white hover:bg-blue-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      >
+                        Submit
+                      </button>
+                      <button
+                        type="submit"
+                        onClick={closeModal}
+                        className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      >
+                        Cancel
+                      </button>
+                    </form>
+                  </div>
                 </Dialog.Panel>
               </Transition.Child>
             </div>
