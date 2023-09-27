54ca5b5e231b1a35ae1feecb015bdea606b0f334
sriharsh05 <sriharsh.harsha5@gmail.com>
2023-09-27 16:03:55 +0530

    resolve the bot rejection at delete members

--- smarter-tasks/src/pages/members/MemberListItems.tsx
+++ smarter-tasks/src/pages/members/MemberListItems.tsx
@@ -24,15 +24,14 @@ export default function MemberListItems() {
       {members.map((member: any) => (
         <div
           key={member.id}
-          className="block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+          className="suspense-loading member block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
         >
-          <div>
-            <h5 className="mb-2 text-xl font-medium tracking-tight text-gray-900 dark:text-white">
+          <div className="flex justify-between items-center">
+            <h5 className="mb-1 text-xl font-medium tracking-tight text-gray-900 dark:text-white">
               {member.name}
             </h5>
-
             <button onClick={() => deleteMember(dispatchMembers, member.id)}>
-              <svg
+            <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
@@ -48,7 +47,9 @@ export default function MemberListItems() {
               </svg>
             </button>
           </div>
-          <p className="text-gray-900">{member.email}</p>
+          <p className="mb-2 text-lg font-medium tracking-tight text-gray-400 dark:text-white">
+            {member.email}
+          </p>
         </div>
       ))}
     </>
