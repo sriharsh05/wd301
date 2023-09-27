43d4afcd92f8c2879df587b6bb4253d076aa03a2
sriharsh05 <sriharsh.harsha5@gmail.com>
2023-09-27 11:11:45 +0530

    implement react-hook form in sign-in page

--- smarter-tasks/src/pages/signin/SigninForm.tsx
+++ smarter-tasks/src/pages/signin/SigninForm.tsx
@@ -1,16 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { API_ENDPOINT } from '../../config/constants';
 import { useNavigate } from "react-router-dom";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  email: string,
+  password: string
+};
 
 
 const Signin: React.FC = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const navigate = useNavigate();
+  const { register, handleSubmit, formState: { errors } } = useForm<Inputs>();
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    const { email, password } = data
     try {
       const response = await fetch(`${API_ENDPOINT}/users/sign_in`, {
         method: 'POST',
@@ -25,28 +29,30 @@ const Signin: React.FC = () => {
       console.log('Sign-in successful');
       const data = await response.json();
 
-      // After successful signin, first we will save the token in localStorage
       localStorage.setItem('authToken', data.token);
       localStorage.setItem('userData', JSON.stringify(data.user));
       navigate("/account");
       
-      // After successful signin we have to redirect the user to the secured page. We will do that later.
-
     } catch (error) {
       console.error('Sign-in failed:', error);
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div>
         <label className="block text-gray-700 font-semibold mb-2">Email:</label>
-        <input type="email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue" />
+        <input type="email" id="email" {...register('email', { required: true })} autoFocus  className={`w-full border rounded-md py-2 px-3 my-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue ${
+    errors.email ? 'border-red-500' : ''
+  }`} />
+  {errors.email && <span>This field is required</span>}
       </div>
       <div>
         <label className="block text-gray-700 font-semibold mb-2">Password:</label>
-        <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue" />
-      </div>
+        <input type="password" id="password" {...register('password', { required: true })} autoFocus  className={`w-full border rounded-md py-2 px-3 my-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue ${
+    errors.password ? 'border-red-500' : ''
+  }`} />
+  {errors.password && <span>This field is required</span>}      </div>
       <button type="submit" className="w-full bg-gray-700 hover:bg-gray-800 text-white font-semibold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline-gray mt-4">Sign In</button>
     </form>
   );
