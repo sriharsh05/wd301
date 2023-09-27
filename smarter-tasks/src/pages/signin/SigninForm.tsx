74e8fb19de009446a097531fc36f803f9e31817c
sriharsh05 <sriharsh.harsha5@gmail.com>
2023-09-27 11:25:51 +0530

    add react-hook-form to signup form

--- smarter-tasks/src/pages/signin/SigninForm.tsx
+++ smarter-tasks/src/pages/signin/SigninForm.tsx
@@ -1,40 +1,42 @@
-import React from 'react';
-import { API_ENDPOINT } from '../../config/constants';
+import React from "react";
+import { API_ENDPOINT } from "../../config/constants";
 import { useNavigate } from "react-router-dom";
 import { useForm, SubmitHandler } from "react-hook-form";
 
 type Inputs = {
-  email: string,
-  password: string
+  email: string;
+  password: string;
 };
-
 
 const Signin: React.FC = () => {
   const navigate = useNavigate();
-  const { register, handleSubmit, formState: { errors } } = useForm<Inputs>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
 
   const onSubmit: SubmitHandler<Inputs> = async (data) => {
-    const { email, password } = data
+    const { email, password } = data;
     try {
       const response = await fetch(`${API_ENDPOINT}/users/sign_in`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
       });
 
       if (!response.ok) {
-        throw new Error('Sign-in failed');
+        throw new Error("Sign-in failed");
       }
 
-      console.log('Sign-in successful');
+      console.log("Sign-in successful");
       const data = await response.json();
 
-      localStorage.setItem('authToken', data.token);
-      localStorage.setItem('userData', JSON.stringify(data.user));
+      localStorage.setItem("authToken", data.token);
+      localStorage.setItem("userData", JSON.stringify(data.user));
       navigate("/account");
-      
     } catch (error) {
-      console.error('Sign-in failed:', error);
+      console.error("Sign-in failed:", error);
     }
   };
 
@@ -42,18 +44,38 @@ const Signin: React.FC = () => {
     <form onSubmit={handleSubmit(onSubmit)}>
       <div>
         <label className="block text-gray-700 font-semibold mb-2">Email:</label>
-        <input type="email" id="email" {...register('email', { required: true })} autoFocus  className={`w-full border rounded-md py-2 px-3 my-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue ${
-    errors.email ? 'border-red-500' : ''
-  }`} />
-  {errors.email && <span>This field is required</span>}
+        <input
+          type="email"
+          id="email"
+          {...register("email", { required: true })}
+          autoFocus
+          className={`w-full border rounded-md py-2 px-3 my-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue ${
+            errors.email ? "border-red-500" : ""
+          }`}
+        />
+        {errors.email && <span>This field is required</span>}
       </div>
       <div>
-        <label className="block text-gray-700 font-semibold mb-2">Password:</label>
-        <input type="password" id="password" {...register('password', { required: true })} autoFocus  className={`w-full border rounded-md py-2 px-3 my-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue ${
-    errors.password ? 'border-red-500' : ''
-  }`} />
-  {errors.password && <span>This field is required</span>}      </div>
-      <button type="submit" className="w-full bg-gray-700 hover:bg-gray-800 text-white font-semibold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline-gray mt-4">Sign In</button>
+        <label className="block text-gray-700 font-semibold mb-2">
+          Password:
+        </label>
+        <input
+          type="password"
+          id="password"
+          {...register("password", { required: true })}
+          autoFocus
+          className={`w-full border rounded-md py-2 px-3 my-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue ${
+            errors.password ? "border-red-500" : ""
+          }`}
+        />
+        {errors.password && <span>This field is required</span>}{" "}
+      </div>
+      <button
+        type="submit"
+        className="w-full bg-gray-700 hover:bg-gray-800 text-white font-semibold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline-gray mt-4"
+      >
+        Sign In
+      </button>
     </form>
   );
 };
--- smarter-tasks/src/pages/signup/SignupForm.tsx
+++ smarter-tasks/src/pages/signup/SignupForm.tsx
@@ -1,59 +1,111 @@
-import React, { useState } from 'react';
-import { API_ENDPOINT } from '../../config/constants';
-import { useNavigate } from "react-router-dom";
+import React from "react";
+import { API_ENDPOINT } from "../../config/constants";
+import { Link, useNavigate } from "react-router-dom";
+import { useForm, SubmitHandler } from "react-hook-form";
 
+type Inputs = {
+  organisationName: string;
+  userName: string;
+  userEmail: string;
+  userPassword: string;
+};
 
 const SignupForm: React.FC = () => {
-  const [organisationName, setOrganisationName] = useState('');
-  const [userName, setUserName] = useState('');
-  const [userEmail, setUserEmail] = useState('');
-  const [userPassword, setUserPassword] = useState('');
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
   const navigate = useNavigate();
 
-
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    const { organisationName, userName, userEmail, userPassword } = data;
 
     try {
       const response = await fetch(`${API_ENDPOINT}/organisations`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ name: organisationName, user_name: userName, email: userEmail, password: userPassword}),
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          name: organisationName,
+          user_name: userName,
+          email: userEmail,
+          password: userPassword,
+        }),
       });
 
       if (!response.ok) {
-        throw new Error('Sign-up failed');
+        throw new Error("Sign-up failed");
       }
-      console.log('Sign-up successful');
-      const data = await response.json();
-
-    localStorage.setItem('authToken', data.token);
-    localStorage.setItem('userData', JSON.stringify(data.user))
-    navigate("/account");
+      console.log("Sign-up successful");
+      navigate("/account");
     } catch (error) {
-      console.error('Sign-up failed:', error);
+      console.error("Sign-up failed:", error);
     }
   };
-  
+
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div>
-        <label className="block text-gray-700 font-semibold mb-2">Organisation Name:</label>
-        <input type="text" name="organisationName" id="organisationName" value={organisationName} onChange={(e) => setOrganisationName(e.target.value)} className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue" />
+        <label className="block text-gray-700 font-semibold mb-2">
+          Organisation Name:
+        </label>
+        <input
+          type="text"
+          id="organisationName"
+          {...register("organisationName", { required: true })}
+          className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue"
+        />
+        {errors.organisationName && (
+          <span className="text-red-500">Organisation Name is required</span>
+        )}
       </div>
       <div>
-        <label className="block text-gray-700 font-semibold mb-2">Your Name:</label>
-        <input type="text" name="userName" id="userName" value={userName} onChange={(e) => setUserName(e.target.value)} className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue" />
+        <label className="block text-gray-700 font-semibold mb-2">
+          Your Name:
+        </label>
+        <input
+          type="text"
+          id="userName"
+          {...register("userName", { required: true })}
+          className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue"
+        />
+        {errors.userName && (
+          <span className="text-red-500">Name is required</span>
+        )}
       </div>
       <div>
         <label className="block text-gray-700 font-semibold mb-2">Email:</label>
-        <input type="email" name="userEmail" id="userEmail" value={userEmail} onChange={(e) => setUserEmail(e.target.value)} className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue" />
+        <input
+          type="email"
+          id="userEmail"
+          {...register("userEmail", { required: true })}
+          className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue"
+        />
+        {errors.userEmail && (
+          <span className="text-red-500">Email is required</span>
+        )}
       </div>
       <div>
-        <label className="block text-gray-700 font-semibold mb-2">Password:</label>
-        <input type="password" name="userPassword" id="userPassword" value={userPassword} onChange={(e) => setUserPassword(e.target.value)} className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue" />
+        <label className="block text-gray-700 font-semibold mb-2">
+          Password:
+        </label>
+        <input
+          type="password"
+          id="userPassword"
+          {...register("userPassword", { required: true })}
+          className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue"
+        />
+        {errors.userPassword && (
+          <span className="text-red-500">Password is required</span>
+        )}
       </div>
-      <button type="submit" className="w-full bg-gray-700 hover:bg-gray-800 text-white font-semibold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline-gray mt-4">Sign up</button>
+      <button
+        type="submit"
+        className="w-full mb-2 bg-gray-700 hover:bg-gray-800 text-white font-semibold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline-gray mt-4"
+      >
+        Sign up
+      </button>
+      <Link to={"/signin"} className="hover:text-blue-500 transition-all">Already have an account? Sign in</Link>
     </form>
   );
 };
